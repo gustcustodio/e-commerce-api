c1d08696c248e380edb146fad6fb717526e36dbe
gustcustodio <gustcustodio@gmail.com>
2026-04-12 14:08:10 -0300

    create ControllerExceptionHandler

---
+++ src/main/java/com/gustcustodio/e_commerce_api/controllers/handler/ControllerExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.gustcustodio.e_commerce_api.controllers.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    
+
+}
