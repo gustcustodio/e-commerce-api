cbaac5a307fcc8c01828ce0903a4affd73335d49
gustcustodio <gustcustodio@gmail.com>
2026-04-26 09:56:50 -0300

    create ResourceNotFoundException

--- src/main/java/com/gustcustodio/e_commerce_api/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/gustcustodio/e_commerce_api/services/exceptions/ResourceNotFoundException.java
@@ -2,6 +2,10 @@ package com.gustcustodio.e_commerce_api.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
     public ResourceNotFoundException() {
         super("Resource not found");
     }
