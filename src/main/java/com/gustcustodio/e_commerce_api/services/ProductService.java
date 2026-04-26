06228dd6d0108dcaaf9b283c10ef2e786f8f8767
gustcustodio <gustcustodio@gmail.com>
2026-04-26 10:10:32 -0300

    refactor ProductService

--- src/main/java/com/gustcustodio/e_commerce_api/services/ProductService.java
+++ src/main/java/com/gustcustodio/e_commerce_api/services/ProductService.java
@@ -1,10 +1,15 @@
 package com.gustcustodio.e_commerce_api.services;
 
+import com.gustcustodio.e_commerce_api.dtos.CategoryResponseDTO;
 import com.gustcustodio.e_commerce_api.dtos.ProductRequestDTO;
 import com.gustcustodio.e_commerce_api.dtos.ProductResponseDTO;
+import com.gustcustodio.e_commerce_api.entities.Category;
 import com.gustcustodio.e_commerce_api.entities.Product;
+import com.gustcustodio.e_commerce_api.repositories.CategoryRepository;
 import com.gustcustodio.e_commerce_api.repositories.ProductRepository;
+import com.gustcustodio.e_commerce_api.services.exceptions.DatabaseException;
 import com.gustcustodio.e_commerce_api.services.exceptions.ResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,14 +20,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Transactional(readOnly = true)
     public ProductResponseDTO findProductWithCategories(Long id) {
-        Product entity = productRepository.findProductWithCategories(id).orElseThrow(() -> new ResourceNotFoundException());
+        Product entity = productRepository.findProductWithCategories(id).orElseThrow(ResourceNotFoundException::new);
         return new ProductResponseDTO(entity);
     }
 
@@ -43,7 +50,7 @@ public class ProductService {
 
     @Transactional
     public ProductResponseDTO updateProduct(Long id, ProductRequestDTO productRequestDTO) {
-        Product entity = productRepository.findProductWithCategories(id).orElseThrow(() -> new ResourceNotFoundException());
+        Product entity = productRepository.findProductWithCategories(id).orElseThrow(ResourceNotFoundException::new);
         convertDtoToEntity(productRequestDTO, entity);
         entity = productRepository.save(entity);
         return new ProductResponseDTO(entity);
@@ -52,7 +59,11 @@ public class ProductService {
     @Transactional(propagation = Propagation.SUPPORTS)
     public void deleteProduct(Long id) {
         if (!productRepository.existsById(id)) throw new ResourceNotFoundException();
-        productRepository.deleteById(id);
+        try {
+            productRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException();
+        }
     }
 
     public void convertDtoToEntity(ProductRequestDTO productRequestDTO, Product entity) {
@@ -60,6 +71,11 @@ public class ProductService {
         entity.setDescription(productRequestDTO.description());
         entity.setPrice(productRequestDTO.price());
         entity.setQuantity(productRequestDTO.quantity());
+        entity.getCategories().clear();
+        for (CategoryResponseDTO categoryResponseDTO : productRequestDTO.categories()) {
+            Category category = categoryRepository.getReferenceById(categoryResponseDTO.id());
+            entity.getCategories().add(category);
+        }
     }
 
 }
