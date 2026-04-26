ab8ccbfc6842ce9fefc397b59bf6eefad28d6fab
gustcustodio <gustcustodio@gmail.com>
2026-04-26 12:17:18 -0300

    create ValidationErrorDTO

---
+++ src/main/java/com/gustcustodio/e_commerce_api/dtos/ValidationErrorDTO.java
@@ -0,0 +1,23 @@
+package com.gustcustodio.e_commerce_api.dtos;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrorDTO extends CustomErrorDTO {
+
+    List<FieldMessageDTO> errors = new ArrayList<>();
+
+    public ValidationErrorDTO(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessageDTO> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(String fieldName, String message) {
+        errors.add(new FieldMessageDTO(fieldName, message));
+    }
+
+}
