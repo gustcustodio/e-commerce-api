ac52388a43f3a4cfbbd8c86aea5221d7a4f29a94
gustcustodio <gustcustodio@gmail.com>
2026-04-26 10:09:48 -0300

    create Factory

---
+++ src/test/java/com/gustcustodio/e_commerce_api/constructors/Factory.java
@@ -0,0 +1,33 @@
+package com.gustcustodio.e_commerce_api.constructors;
+
+import com.gustcustodio.e_commerce_api.dtos.CategoryResponseDTO;
+import com.gustcustodio.e_commerce_api.dtos.ProductRequestDTO;
+import com.gustcustodio.e_commerce_api.dtos.ProductResponseDTO;
+import com.gustcustodio.e_commerce_api.entities.Category;
+import com.gustcustodio.e_commerce_api.entities.Product;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product = new Product(1L, "Valid Product", "Valid Product Description", 100.0, 10);
+        product.getCategories().add(createCategory());
+        return product;
+    }
+
+    public static ProductRequestDTO createProductRequestDTO() {
+        return new ProductRequestDTO(createProduct());
+    }
+
+    public static ProductResponseDTO createProductResponseDTO() {
+        return new ProductResponseDTO(createProduct());
+    }
+
+    public static Category createCategory() {
+        return new Category(1L, "Books");
+    }
+
+    public static CategoryResponseDTO createCategoryResponseDTO() {
+        return new CategoryResponseDTO(createCategory());
+    }
+
+}
