72b3e070b911053d6529c227169b60e06c23d57b
gustcustodio <gustcustodio@gmail.com>
2026-04-26 10:10:38 -0300

    refactor ProductRequestDTO

--- src/main/java/com/gustcustodio/e_commerce_api/dtos/ProductRequestDTO.java
+++ src/main/java/com/gustcustodio/e_commerce_api/dtos/ProductRequestDTO.java
@@ -1,5 +1,20 @@
 package com.gustcustodio.e_commerce_api.dtos;
 
-public record ProductRequestDTO(String name, String description, Double price, Integer quantity) {
+import com.gustcustodio.e_commerce_api.entities.Product;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record ProductRequestDTO(String name, String description, Double price, Integer quantity, Set<CategoryResponseDTO> categories) {
+
+    public ProductRequestDTO(Product entity) {
+        this(
+                entity.getName(),
+                entity.getDescription(),
+                entity.getPrice(),
+                entity.getQuantity(),
+                entity.getCategories().stream().map(CategoryResponseDTO::new).collect(Collectors.toSet())
+        );
+    }
 
 }
