3334764047a7c0eb9ba2c2610c1ef31aad64bb9f
gustcustodio <gustcustodio@gmail.com>
2026-05-16 06:29:30 -0300

    create CategoryRequestDTO

---
+++ src/main/java/com/gustcustodio/e_commerce_api/dtos/CategoryRequestDTO.java
@@ -0,0 +1,13 @@
+package com.gustcustodio.e_commerce_api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoryRequestDTO(
+        @NotBlank(message = "Name is required")
+        @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+        String name
+) {
+
+}
+
