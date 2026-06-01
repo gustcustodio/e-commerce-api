2541b7a3f779281341ecc480295979c9087dda09
gustcustodio <gustcustodio@gmail.com>
2026-06-01 06:55:35 -0300

    create UserController with CRUD

---
+++ src/main/java/com/gustcustodio/e_commerce_api/controllers/UserController.java
@@ -0,0 +1,56 @@
+package com.gustcustodio.e_commerce_api.controllers;
+
+import com.gustcustodio.e_commerce_api.dtos.UserRequestDTO;
+import com.gustcustodio.e_commerce_api.dtos.UserResponseDTO;
+import com.gustcustodio.e_commerce_api.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserResponseDTO> findUserById(@PathVariable Long id) {
+        UserResponseDTO dto = userService.findUserById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> findAllUsers(@RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+                                                              @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize) {
+        Page<UserResponseDTO> page = userService.findAllUsers(pageNumber, pageSize);
+        return ResponseEntity.ok(page);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO userResponseDTO = userService.createUser(userRequestDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userResponseDTO.id()).toUri();
+        return ResponseEntity.created(uri).body(userResponseDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO userResponseDTO = userService.updateUser(id, userRequestDTO);
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
