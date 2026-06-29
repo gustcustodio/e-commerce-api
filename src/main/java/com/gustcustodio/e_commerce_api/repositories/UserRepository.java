b7e6cb23cafa164110b52ab3f97c45ad14ddba97
gustcustodio <gustcustodio@gmail.com>
2026-06-29 06:53:39 -0300

    add custom query to UserRepository for fetching user by email with roles

--- src/main/java/com/gustcustodio/e_commerce_api/repositories/UserRepository.java
+++ src/main/java/com/gustcustodio/e_commerce_api/repositories/UserRepository.java
@@ -2,7 +2,7 @@ package com.gustcustodio.e_commerce_api.repositories;
 
 import com.gustcustodio.e_commerce_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +10,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT user FROM User user " +
+            "JOIN FETCH user.roles " +
+            "WHERE user.email = :email")
     Optional<User> findByEmail(String email);
 
 }
