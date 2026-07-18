03ac539f4d7643499d824fdd5a27afe7309a9168
gustcustodio <gustcustodio@gmail.com>
2026-07-18 18:42:49 -0300

    add active status to user creation methods

--- src/test/java/com/gustcustodio/e_commerce_api/factories/UserFactory.java
+++ src/test/java/com/gustcustodio/e_commerce_api/factories/UserFactory.java
@@ -6,7 +6,7 @@ import com.gustcustodio.e_commerce_api.entities.User;
 public class UserFactory {
 
     public static User createUserAdminAndClient() {
-        User user = new User(1L, "Valid User", "391.385.040-65", "991233344", "user@email.com", "$2a$10$VDNktmwwHMKRm8lUbdaHw.oYJbkU6hX3laW1pkyx7m7fb69QMdeJe");
+        User user = new User(1L, "Valid User", "391.385.040-65", "991233344", "user@email.com", "$2a$10$VDNktmwwHMKRm8lUbdaHw.oYJbkU6hX3laW1pkyx7m7fb69QMdeJe", true);
         Role roleAdmin = new Role(1L, "ROLE_ADMIN");
         Role roleClient = new Role(2L, "ROLE_CLIENT");
         user.getRoles().add(roleAdmin);
@@ -15,14 +15,14 @@ public class UserFactory {
     }
 
     public static User createUserAdmin() {
-        User user = new User(2L, "Valid Admin User", "441.766.180-43", "991234455", "admin@email.com", "$2a$10$VDNktmwwHMKRm8lUbdaHw.oYJbkU6hX3laW1pkyx7m7fb69QMdeJe");
+        User user = new User(2L, "Valid Admin User", "441.766.180-43", "991234455", "admin@email.com", "$2a$10$VDNktmwwHMKRm8lUbdaHw.oYJbkU6hX3laW1pkyx7m7fb69QMdeJe", true);
         Role role = new Role(1L, "ROLE_ADMIN");
         user.getRoles().add(role);
         return user;
     }
 
     public static User createUserClient() {
-        User user = new User(3L, "Valid Client User", "563.557.910-86", "991235566", "client@email.com", "$2a$10$VDNktmwwHMKRm8lUbdaHw.oYJbkU6hX3laW1pkyx7m7fb69QMdeJe");
+        User user = new User(3L, "Valid Client User", "563.557.910-86", "991235566", "client@email.com", "$2a$10$VDNktmwwHMKRm8lUbdaHw.oYJbkU6hX3laW1pkyx7m7fb69QMdeJe", true);
         Role role = new Role(2L, "ROLE_CLIENT");
         user.getRoles().add(role);
         return user;
