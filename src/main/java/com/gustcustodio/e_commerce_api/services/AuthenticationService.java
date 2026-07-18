a5c0e163f9cbc79fec08aaa71bc7f300563148f5
gustcustodio <gustcustodio@gmail.com>
2026-07-18 16:51:09 -0300

    add @Transactional annotation to register and login methods

--- src/main/java/com/gustcustodio/e_commerce_api/services/AuthenticationService.java
+++ src/main/java/com/gustcustodio/e_commerce_api/services/AuthenticationService.java
@@ -21,6 +21,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthenticationService {
@@ -39,6 +40,7 @@ public class AuthenticationService {
         this.tokenProvider = tokenProvider;
     }
 
+    @Transactional
     public RegisterResponseDTO register(RegisterRequestDTO registerRequestDTO) {
         User user = new User();
         copyDtoToEntity(registerRequestDTO, user);
@@ -48,6 +50,7 @@ public class AuthenticationService {
         return new RegisterResponseDTO(user);
     }
 
+    @Transactional
     public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
         try {
             Authentication authentication =
